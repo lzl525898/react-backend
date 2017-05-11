cec89683d40302631eca6d1a26d9c2d16e036124
liangzelei <luckyforlei@163.com>
2017-05-11 16:58:36 +0800

    update

--- src/js/components/back-user-batch-list.js
+++ src/js/components/back-user-batch-list.js
@@ -1,17 +1,77 @@
 import React, { Component } from 'react';
 
 import {
-  Icon
+  Icon,
+  Transfer
 } from 'antd';
 
 export default class BatcgUserlist extends Component {
   constructor(props){
     super(props);
     this.state={
-      isShowUserList: false
+      isShowUserList: true,
+      mockData: [],
+      targetKeys: [],
     };
   };
+  componentDidMount() {
+    this.getMock();
+  }
+  getMock(){
+    const targetKeys = [];
+    const mockData = [];
+    for (let i = 0; i < 20; i++) {
+      const data = {
+        key: i.toString(),
+        title: `张三${i + 1}`,
+        description: `普通用户${i + 1}`,
+        chosen: Math.random() * 2 > 1,
+      };
+      if (data.chosen) {
+        targetKeys.push(data.key);
+      }
+      mockData.push(data);
+    }
+    this.setState({
+      mockData: mockData,
+      targetKeys: targetKeys
+    });
+  };
+  handleChange(targetKeys, direction, moveKeys){
+    console.log(targetKeys, direction, moveKeys);
+    this.setState({ targetKeys: targetKeys});
+  };
+  renderItem(item){
+    const customLabel = (
+      <span className="custom-item">
+        {item.title} - {item.description}
+      </span>
+    );
+    return {
+      label: customLabel,  // for displayed item
+      value: item.title,   // for title and filter matching
+    };
+  }
   render(){
+    const userlist = this.state.isShowUserList
+    ?
+      <div>
+        <div style={{marginTop:5}}></div>
+        <Transfer
+          dataSource={this.state.mockData}
+          listStyle={{
+            width: 300,
+            height: 300,
+          }}
+          notFoundContent={'未选择任何用户'}
+          titles={['所有用户','所有已选用户']}
+          targetKeys={this.state.targetKeys}
+          onChange={this.handleChange.bind(this)}
+          render={this.renderItem.bind(this)}
+        />
+      </div>
+    :
+      <div></div>;
     return(
       <div style={{ marginTop: 5 }}>
         <div style={{ lineHeight: "18px", marginTop: "5px"}}>
@@ -25,6 +85,7 @@ export default class BatcgUserlist extends Component {
             &nbsp;&nbsp;列表中的用户
           </a>
         </div>
+        { userlist }
       </div>
     )
   }
