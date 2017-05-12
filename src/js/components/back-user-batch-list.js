5b9c3e25d9d7fb8105e7b180573d8ee636987350
liangzelei <luckyforlei@163.com>
2017-05-12 17:27:04 +0800

    update

--- src/js/components/back-user-batch-list.js
+++ src/js/components/back-user-batch-list.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
 
 import {
+  Row,
+  Col,
   Icon,
-  Transfer
+  Transfer,
+  Button,
+  Select
 } from 'antd';
+
+const Option = Select.Option;
 
 export default class BatcgUserlist extends Component {
   constructor(props){
@@ -14,7 +20,7 @@ export default class BatcgUserlist extends Component {
       targetKeys: [],
     };
   };
-  componentDidMount() {
+  componentWillMount() {
     this.getMock();
   }
   getMock(){
@@ -24,7 +30,7 @@ export default class BatcgUserlist extends Component {
       const data = {
         key: i.toString(),
         title: `张三${i + 1}`,
-        description: `普通用户${i + 1}`,
+        description: `普通用户`,
         chosen: Math.random() * 2 > 1,
       };
       if (data.chosen) {
@@ -38,37 +44,72 @@ export default class BatcgUserlist extends Component {
     });
   };
   handleChange(targetKeys, direction, moveKeys){
-    console.log(targetKeys, direction, moveKeys);
+    // console.log(targetKeys, direction, moveKeys);
     this.setState({ targetKeys: targetKeys});
   };
   renderItem(item){
     const customLabel = (
-      <span className="custom-item">
-        {item.title} - {item.description}
-      </span>
+        <span className="custom-item">
+          {item.title} - {item.description}
+        </span>
     );
     return {
       label: customLabel,  // for displayed item
       value: item.title,   // for title and filter matching
     };
+  };
+  handleSelectChange(value){
+    console.log(value);
+  };
+  renderFooter(props){
+    if ('所有用户'==props.titleText) {
+      return (
+        <div>
+          <Button size="small" style={{ float: 'right', margin: 5 }}
+            onClick={this.getMock.bind(this)}
+          >
+            数据还原
+          </Button>
+        </div>
+      );
+    } else { //
+      return (
+        <div>
+          <Button size="small" style={{ float: 'right', margin: 5 }}
+            onClick={this.getMock.bind(this)}
+          >
+            数据还原
+          </Button>
+        </div>
+      );
+    }
   }
   render(){
     const userlist = this.state.isShowUserList
     ?
-      <div>
-        <div style={{marginTop:5}}></div>
-        <Transfer
-          dataSource={this.state.mockData}
-          listStyle={{
-            width: 300,
-            height: 300,
-          }}
-          notFoundContent={'未选择任何用户'}
-          titles={['所有用户','所有已选用户']}
-          targetKeys={this.state.targetKeys}
-          onChange={this.handleChange.bind(this)}
-          render={this.renderItem.bind(this)}
-        />
+      <div style={{height: 320}}>
+        <Row style={{ marginTop: 10 }}>
+          <Col span={1}></Col>
+          <Col span={2} style={{ height: "20px", lineHeight: "20px", marginTop: "3px" }}><span>用户</span></Col>
+          <Col span={21} style={{ height: "20px" }}>
+            <div style={{marginTop:5}}></div>
+            <div>
+              <Transfer
+                dataSource={this.state.mockData}
+                listStyle={{
+                  width: 300,
+                  height: 300,
+                }}
+                notFoundContent={'未选择任何用户'}
+                titles={['所有用户','所有已选用户']}
+                targetKeys={this.state.targetKeys}
+                footer={this.renderFooter.bind(this)}
+                onChange={this.handleChange.bind(this)}
+                render={this.renderItem.bind(this)}
+              />
+            </div>
+          </Col>
+        </Row>
       </div>
     :
       <div></div>;
@@ -86,6 +127,26 @@ export default class BatcgUserlist extends Component {
           </a>
         </div>
         { userlist }
+        <div style={{ borderBottom: "1px solid #cecece"}}></div>
+        <div>
+          <Row style={{ marginTop: 10 }}>
+            <Col span={3} style={{ height: "20px", lineHeight: "20px", marginTop: "3px" }}>
+              <span><a style={{ textDecoration: "none", fontSize: "18px" }}>
+                对选中的用户...
+                </a></span>
+            </Col>
+            <Col span={21} style={{ height: "20px" }}>
+              <div>
+              <Select defaultValue="选择..." style={{ width: 120 }} onChange={this.handleSelectChange.bind(this)}>
+                <Option value="message">发消息</Option>
+                <Option value="password">强制修改密码</Option>
+                <Option value="download">下载</Option>
+              </Select>
+              <Button style={{marginLeft: 20}} type='primary'>继续</Button>
+              </div>
+            </Col>
+          </Row>
+        </div>
       </div>
     )
   }
